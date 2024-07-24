669d693b2496d503612d3c2e123c24fb303d2fd7
thanasinb <55657279+thanasinb@users.noreply.github.com>
2024-07-24 21:33:19 +0700

    Create lab2_interrupt_count_debounce.cpp

---
+++ lab2_interrupt_count_debounce.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+IRAM_ATTR void handleInterrupt();
+const int IDLE = 0;
+const int PRESS = 1;
+int flag = false;
+int count = -1;
+int state;
+void setup() { 
+  Serial.begin(115200);
+	state = IDLE;
+	pinMode(D4, OUTPUT); 
+	pinMode(D6, INPUT); 
+	attachInterrupt(digitalPinToInterrupt(D6), handleInterrupt, RISING); 
+} 
+void loop() { 
+	if (state == IDLE){
+		digitalWrite(D4, HIGH); 
+		if (flag == true){
+  		flag = false;
+      delay(50);
+      if(digitalRead(D6)==1){
+  			state = PRESS;
+      }
+		}
+	}else if (state == PRESS){
+    count++;
+    if (count > 9){
+      count = 0;
+    }
+    Serial.println(count);
+    state = IDLE;
+	}
+} 
+IRAM_ATTR void handleInterrupt() { 
+	flag = true;
+}
